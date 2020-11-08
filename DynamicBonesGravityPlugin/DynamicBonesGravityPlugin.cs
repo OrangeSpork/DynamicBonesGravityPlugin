b24f8912e007bae3513a6cc21099c371069dd207
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2020-11-08 14:39:42 -0700

    Alternate Update Mode

--- DynamicBonesGravityPlugin/DynamicBonesGravityPlugin.cs
+++ DynamicBonesGravityPlugin/DynamicBonesGravityPlugin.cs
@@ -23,6 +23,7 @@ namespace DynamicBonesGravityPlugin
         public static ConfigEntry<bool> RealismEnabled { get; set; }
         public static ConfigEntry<bool> AdvancedModeEnabled { get; set; }
         public static ConfigEntry<float> AdvancedGravityAdjustment { get; set; }
+        public static ConfigEntry<bool> AlternateUpdateMode { get; set; }
 
         public static DynamicBonesGravityPlugin Instance { get; set; }
 
@@ -42,12 +43,17 @@ namespace DynamicBonesGravityPlugin
             RealismEnabled = Config.Bind("Config", "Realistic!!!", false, new ConfigDescription("Realish (Compensates for 10x size with 10 Times Default)", null, new ConfigurationManagerAttributes { Order = 3 }));
             AdvancedModeEnabled = Config.Bind("Config", "Advanced Mode", false, new ConfigDescription("Use the values below instead of defaults, control your own destiny!", null, new ConfigurationManagerAttributes { Order = 2 }));
             AdvancedGravityAdjustment = Config.Bind("Config", "Advanced Gravity", -0.015f, new ConfigDescription("Y Gravity Adjustment", new AcceptableValueRange<float>(-.1f, -0.001f), new ConfigurationManagerAttributes { Order = 1 }));
+            AlternateUpdateMode = Config.Bind("Config", "Alternate Update Mode", false, new ConfigDescription("Calc in FixedUpdate Instead of LateUpdate", null, new ConfigurationManagerAttributes {  Order = -1 }));
 
             Config.SettingChanged += ConfigUpdated;
 
             Harmony harmony = new Harmony(GUID);
             MethodInfo overrideMethod = AccessTools.Method(typeof(DynamicBone), "Start", null, null);
-            harmony.Patch(overrideMethod, new HarmonyMethod(typeof(DynamicBonesGravityPlugin), "OverrideDynamicBonesGravity"), null, null, null);          
+            harmony.Patch(overrideMethod, new HarmonyMethod(typeof(DynamicBonesGravityPlugin), "OverrideDynamicBonesGravity"), null, null, null);
+            MethodInfo dynamicBoneFixedUpdateMethod = AccessTools.Method(typeof(DynamicBone), "FixedUpdate");
+            harmony.Patch(dynamicBoneFixedUpdateMethod, new HarmonyMethod(typeof(DynamicBonesGravityPlugin), "DynamicBoneFixedUpdateOverride"), null, null, null);
+            MethodInfo dyanmicBoneLateUpdateMethod = AccessTools.Method(typeof(DynamicBone), "LateUpdate");
+            harmony.Patch(dyanmicBoneLateUpdateMethod, new HarmonyMethod(typeof(DynamicBonesGravityPlugin), "DynamicBoneLateUpdateOverride"), null, null, null);
         }        
 
        
@@ -116,6 +122,46 @@ namespace DynamicBonesGravityPlugin
             {
                 UpdateBone(dynamicBone, configChange);
             }
+        }
+
+
+        private static FieldInfo m_distantDisableField = AccessTools.Field(typeof(DynamicBone), "m_DistantDisable");
+        private static FieldInfo m_distantDisabledField = AccessTools.Field(typeof(DynamicBone), "m_DistantDisabled");
+        private static FieldInfo m_weightField = AccessTools.Field(typeof(DynamicBone), "m_Weight");
+        private static MethodInfo preUpdateMethod = AccessTools.Method(typeof(DynamicBone), "PreUpdate");
+        private static MethodInfo checkDistanceMethod = AccessTools.Method(typeof(DynamicBone), "CheckDistance");
+        private static MethodInfo updateDynamicBonesMethod = AccessTools.Method(typeof(DynamicBone), "UpdateDynamicBones");
+
+        public static bool DynamicBoneFixedUpdateOverride(DynamicBone __instance)
+        {
+            if (!AlternateUpdateMode.Value)
+            {
+                return true;
+            }
+
+            float m_Weight = (float)m_weightField.GetValue(__instance);
+            bool m_DistantDisable = (bool)m_distantDisableField.GetValue(__instance);
+            bool m_DistantDisabled = (bool)m_distantDisabledField.GetValue(__instance);
+
+            preUpdateMethod.Invoke(__instance, null);
+            if (m_DistantDisable)
+            {
+                checkDistanceMethod.Invoke(__instance, null);
+            }
+            if (m_Weight > 0f && (!m_DistantDisable || !m_DistantDisabled))
+            {
+                updateDynamicBonesMethod.Invoke(__instance, new object[] { Time.fixedDeltaTime });
+            }
+            return false;
+        }
+
+        public static bool DynamicBoneLateUpdateOverride(DynamicBone __instance)
+        {
+            if (!AlternateUpdateMode.Value)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
